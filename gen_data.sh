518dc0b8d48dd033d9ad2603ee783c3202917dcd
nekocode <nekocode.cn@gmail.com>
2018-02-23 14:50:08 +0800

    Tweaks on the gen_data sh script

--- gen_data.sh
+++ gen_data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [ "$1" == "list_txt" ]; then
+if [ "$1" == "list-txt" ]; then
    ./data/generator.py --token="$(< token)" --list_txt_out=data/list_data.txt --sort_by=stars
-elif [ "$1" == "list_js" ]; then
+elif [ "$1" == "list-js" ]; then
    ./data/generator.py --token="$(< token)" --list_txt_in=data/list_data.txt --list_js_out=data/list_data.js
-elif [ "$1" == "ui_js" ]; then
+elif [ "$1" == "ui-js" ]; then
    ./data/generator.py --token="$(< token)" --ui_js_out=data/ui_data.js
 fi
