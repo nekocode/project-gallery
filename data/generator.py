fc36c59f9598765607752bd35cc06fcc25377b02
nekocode <nekocode.cn@gmail.com>
2018-02-23 14:33:45 +0800

    Tweaks on the data-generator

--- data/generator.py
+++ data/generator.py
@@ -6,22 +6,23 @@ import getopt
 import sys
 
 
-def str_user(avatar, name, url):
+def str_ui(name, url):
     return """
-var user_data = {
-    "avatar": "%s",
-    "name": "%s",
-    "url": "%s",
+var UI_DATA = {
+    "title": "%s",
+    "github": "%s",
+    "description": "Here are the projects I have open sourced.",
+    "footer": "%s"
 }
-"""[1:] % (avatar, name, url)
+"""[1:] % (name, url, name)
 
 
-def str_items(items):
+def str_wrap_list(list_str):
     return """
-var items_data = [
+var LIST_DATA = [
 %s
 ]
-"""[1:] % items
+"""[1:] % list_str
 
 
 def str_category_item(name):
@@ -47,7 +48,14 @@ def str_repo_item(name, description, stars, forks, lang, url):
 """[1:] % (name, description, stars, forks, lang, url)
 
 
-def gen_data_txt(github, out_file_path, sort_by=None):
+def gen_ui_data_js(github, out_file_path):
+    user = github.get_user()
+
+    with open(out_file_path, 'w') as f:
+        f.write(str_ui(user.name, user.html_url))
+
+
+def gen_items_data_txt(github, out_file_path, sort_by=None):
     repos = github.get_user().get_repos("owner")
     if sort_by is not None:
         if sort_by == "stars":
@@ -61,7 +69,7 @@ def gen_data_txt(github, out_file_path, sort_by=None):
             f.write("%s, %d, %d\n" % (repo.name, repo.stargazers_count, repo.forks_count))
 
 
-def gen_data_js(github, in_file_path, out_file_path):
+def gen_items_data_js(github, in_file_path, out_file_path):
     if not os.path.exists(in_file_path):
         return
 
@@ -73,58 +81,68 @@ def gen_data_js(github, in_file_path, out_file_path):
         def write(content):
             js.write(content.encode('utf-8'))
 
-        write(str_user(user.avatar_url, user.name, user.html_url) + "\n")
+        # write(str_user(user.avatar_url, user.name, user.html_url) + "\n")
 
-        items_str = ""
+        list_str = ""
         for line in txt:
             line = line.strip()
             if line.startswith("//"):
                 category_name = line[2:].lstrip()
-                items_str += str_category_item(category_name)
+                list_str += str_category_item(category_name)
 
             elif len(line) > 0:
                 repo_name = line.split(",")[0].rstrip()
                 if repo_name in repos:
                     repo = repos[repo_name]
-                    items_str += str_repo_item(
+                    list_str += str_repo_item(
                         repo.name, repo.description,
                         repo.stargazers_count, repo.forks_count, repo.language,
                         repo.html_url
                     )
 
-        write(str_items(items_str))
+        write(str_wrap_list(list_str))
 
 
 def main():
     def print_usage():
         print("usage: %s --token=GITHUB_TOKEN \n\t" % __file__ +
-              "(--txt_out=TXT_OUT [--sort_by=stars|name]) | \n\t" +
-              "(--txt_in=TXT_IN --js_out=JS_OUT)")
+              "(--list_txt_out=TXT_OUT [--sort_by=stars|name]) | \n\t" +
+              "(--list_txt_in=TXT_IN --list_js_out=JS_OUT) | \n\t" +
+              "(--ui_js_out=JS_OUT)"
+              )
 
     opts, args = getopt.getopt(
-        sys.argv[1:], "", ["token=", "txt_out=", "txt_in=", "js_out=", "sort_by="])
+        sys.argv[1:], "", ["token=", "list_txt_out=", "list_txt_in=", "list_js_out=", "sort_by=", "ui_js_out="])
     opts = {opt[0][2:]: opt[1] for opt in opts}
     if "token" not in opts:
         print_usage()
         return
 
-    if "txt_out" in opts:
+    if "list_txt_out" in opts:
         github = Github(opts["token"])
         sort_by = "stars"
         if "sort_by" in opts:
             sort_by = opts["sort_by"]
 
-        out_file_path = opts["txt_out"]
+        out_file_path = opts["list_txt_out"]
         print("Generating %s now..." % out_file_path)
-        gen_data_txt(github, out_file_path, sort_by)
+        gen_items_data_txt(github, out_file_path, sort_by)
         print("Finished.")
 
-    elif "txt_in" in opts and "js_out" in opts:
+    elif "list_txt_in" in opts and "list_js_out" in opts:
         github = Github(opts["token"])
 
-        out_file_path = opts["js_out"]
+        out_file_path = opts["list_js_out"]
         print("Generating %s now..." % out_file_path)
-        gen_data_js(github, opts["txt_in"], opts["js_out"])
+        gen_items_data_js(github, opts["list_txt_in"], out_file_path)
+        print("Finished.")
+
+    elif "ui_js_out" in opts:
+        github = Github(opts["token"])
+
+        out_file_path = opts["ui_js_out"]
+        print("Generating %s now..." % out_file_path)
+        gen_ui_data_js(github, out_file_path)
         print("Finished.")
 
     else:
--- gen_data.sh
+++ gen_data.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-if [ "$1" == "txt" ]; then
-   ./data/generator.py --token="$(< token)" --txt_out=data/data.txt --sort_by=stars
-elif [ "$1" == "js" ]; then
-   ./data/generator.py --token="$(< token)" --txt_in=data/data.txt --js_out=data/data.js
+if [ "$1" == "list_txt" ]; then
+   ./data/generator.py --token="$(< token)" --list_txt_out=data/list_data.txt --sort_by=stars
+elif [ "$1" == "list_js" ]; then
+   ./data/generator.py --token="$(< token)" --list_txt_in=data/list_data.txt --list_js_out=data/list_data.js
+elif [ "$1" == "ui_js" ]; then
+   ./data/generator.py --token="$(< token)" --ui_js_out=data/ui_data.js
 fi
